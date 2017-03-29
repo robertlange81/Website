e44d333e702b1a645f3fd615bd45cd9c41f9ab3e
robertlange81 <robert.lange.81@gmail.com>
2017-03-29 21:49:28 +0200

    noch n biss und Santinas Rückkehr

---
+++ sandbox/cv.php
@@ -0,0 +1,156 @@
+
+<html lang="de">
+<head>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">    
+  <title>Curriculum Vitae</title>
+  <meta charset="utf-8">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style>
+
+        .brown {
+            color: #DC143C;
+        }
+        img {
+            border: 1px solid #DC143C;
+            border-right: 2px solid #DC143C;
+            border-bottom: 2px solid #DC143C;
+            background-color: #fff;
+            margin: 0 auto;
+            width: 200px;
+            text-align: center;
+           /* border-radius */
+            border-radius: 20px;
+           /* box-shadow */
+            box-shadow: rgba(0,0,0,0.8) 0 0 10px;
+            border-collapse: collapse;            
+        }
+        
+@media (min-width: 767px) {
+        .content-right {
+          border-left: 1px solid #333;
+        }
+        body {
+          text-align: left;
+          width: 1000px;
+          margin-left: auto;
+          margin-right: auto;
+        }
+        html {
+            background: black;
+        }   
+        .cell {
+            padding-top: 15px !important;
+            padding-left: 50px
+
+        }
+        #inner{
+            margin-left: 50px;
+            margin-right: 20px;
+        }
+}
+@media (max-width: 767px) {
+        body {
+          text-align: center;
+        }
+}
+    </style>
+<body id="seitenanfang" data-spy="scroll" data-target=".navbar" data-offset="60">
+  
+<div id="inner" class="container-fluid bg-4">
+  <h2 style="text-align: center">Curriculum Vitae</h2>
+  <br><br>
+  <div class="row slideanim">
+      <div class="col-sm-8">
+        <div class="row slideanim">
+            <div class="col-sm-6 cell">
+                Name, Vorname
+            </div>
+            <div class="col-sm-6 cell content-right brown">
+                Lange, Robert
+            </div> 
+        </div>
+        <div class="row slideanim">
+            <div class="col-sm-6 cell">
+                Akademische Grade
+            </div>
+            <div class="col-sm-6 cell content-right brown">
+                Diplom - Kaufmann (univ.) <br>
+                Bachelor of Science (FH)
+            </div> 
+        </div>    
+        <div class="row slideanim">
+            <div class="col-sm-6 cell">
+                Staatsangehörigkeit
+            </div>
+            <div class="col-sm-6 cell content-right brown">
+                deutsch
+            </div> 
+        </div>       
+        <div class="row slideanim">
+            <div class="col-sm-6 cell">
+                Geburtsdatum
+            </div>
+            <div class="col-sm-6 cell content-right brown">
+                17.10.1981
+            </div> 
+        </div>   
+                  <div class="row slideanim">
+            <div class="col-sm-6 cell">
+                Akademische Grade
+            </div>
+            <div class="col-sm-6 cell content-right brown">
+                Diplom - Kaufmann (univ.) <br>
+                Bachelor of Science (FH)
+            </div> 
+        </div>   
+        <div class="row slideanim">
+            <div class="col-sm-6 cell">
+                Akademische Grade
+            </div>
+            <div class="col-sm-6 cell content-right brown">
+                Diplom - Kaufmann (univ.) <br>
+                Bachelor of Science (FH)
+            </div> 
+        </div>   
+        <div class="row slideanim">
+            <div class="col-sm-6 cell">
+                Akademische Grade
+            </div>
+            <div class="col-sm-6 cell content-right brown">
+                Diplom - Kaufmann (univ.) <br>
+                Bachelor of Science (FH)
+            </div> 
+        </div>              
+      </div>
+        <div class="col-sm-4">
+          <img style="clear:both;" width="200px" src="images/rl.jpg">
+       </div> 
+  </div> 
+    <div class="row slideanim">
+        <div class="col-sm-4 cell">
+            Akademische Grade
+        </div>
+        <div class="col-sm-8 cell content-right brown">
+            Diplom - Kaufmann (univ.) <br>
+            Bachelor of Science (FH)
+        </div> 
+    </div>      
+<br><br>  
+</div>    
+
+
+<footer class="container-fluid text-center">
+  <a href="#seitenanfang" title="Seitenanfang">
+    <span class="glyphicon glyphicon-chevron-up"></span>
+  </a>
+</footer>
+
+</body>
+</html>
+
