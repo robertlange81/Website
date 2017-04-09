440f0c0d6a500d9231ecd82ec4475bc5fe493d80
robertlange81 <robert.lange.81@gmail.com>
2017-04-09 03:57:11 +0200

    anfang email

--- sandbox/klang/js/creative.js
+++ sandbox/klang/js/creative.js
@@ -16,10 +16,10 @@
         offset: 51
     });
 
-    // Closes the Responsive Menu on Menu Item Click
+    /* Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function() {
         $('.navbar-toggle:visible').click();
-    });
+    });*/
 
     // Offset for Main Navigation
     $('#mainNav').affix({
